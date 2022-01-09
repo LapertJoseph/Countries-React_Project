d3982b6922e9f5e29397eefc78e063265df58942
LapertJoseph <lapertjoseph@gmail.com>
2022-01-09 01:59:08 +0100

    démarrage projet avec création de 3 composant Home, About et NotFound

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="a-propos">
+            <h1>À propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et minus veniam mollitia nulla ratione odit nesciunt. Eius illo iure repellendus molestiae, quaerat quibusdam illum natus dolor! Itaque error odit reiciendis beatae fuga repudiandae culpa eum harum quas doloremque ex, cum nisi tempore magni! Qui deserunt quis, laboriosam animi voluptates tempora adipisci distinctio. Exercitationem sapiente pariatur ab cumque impedit eveniet adipisci?</p>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut deserunt pariatur soluta iste quibusdam quas accusamus ipsam eligendi voluptatem inventore, alias illo dolor corrupti, accusantium non dolorum id quo! Culpa laborum voluptate excepturi dolore dolorem ad molestiae deleniti quaerat sed suscipit atque, illum fugiat blanditiis eaque debitis adipisci ratione maiores iure et modi consequatur animi officia. Mollitia quaerat obcaecati illo cum suscipit eum delectus sunt saepe totam. Similique aspernatur veritatis quam incidunt iusto quae ab! Iure, officia necessitatibus nihil consectetur quisquam alias sapiente non unde quis adipisci tempore sequi fugiat labore reprehenderit. Modi earum nisi delectus sint ut, porro ex adipisci exercitationem nihil, ab officiis qui non possimus deleniti pariatur, nesciunt fugit quis tempore officia? Officiis exercitationem nulla eos non beatae quas quasi explicabo, facilis tempore quidem sit impedit sint, voluptatem enim reprehenderit, blanditiis soluta fugiat tenetur error consequuntur a.</p>
+        </div>
+    );
+};
+
+export default About;
